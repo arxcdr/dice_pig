7b183d4d9ea02f5be532af6dd23ff3b8a3f55fac
arxcdr <34399111+arxcdr@users.noreply.github.com>
2018-09-16 10:02:09 +0200

    Connect scripts to game buttons

---
+++ Dice-Pig/Game.gd
@@ -0,0 +1,15 @@
+extends Node
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _on_DiceButton_pressed():
+	pass
+
+func _on_Left_pressed():
+	pass
+
+func _on_Right_pressed():
+	pass
